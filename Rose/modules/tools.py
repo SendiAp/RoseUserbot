cc3acc23d8509e4c7bd55c805b161a454a1a52bc
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-09-29 02:25:35 +0700

    Create tools.py

---
+++ Rose/modules/tools.py
@@ -0,0 +1,50 @@
+import asyncio
+import math
+import os
+import shlex
+from typing import Tuple
+
+from PIL import Image
+from pymediainfo import MediaInfo
+from pyrogram.types import Message
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+def get_arg(message: Message):
+    msg = message.text
+    msg = msg.replace(" ", "", 1) if msg[1] == " " else msg
+    split = msg[1:].replace("\n", " \n").split(" ")
+    if " ".join(split[1:]).strip() == "":
+        return ""
+    return " ".join(split[1:])
+
+
+def get_args(message: Message):
+    try:
+        message = message.text
+    except AttributeError:
+        pass
+    if not message:
+        return False
+    message = message.split(maxsplit=1)
+    if len(message) <= 1:
+        return []
+    message = message[1]
+    try:
+        split = shlex.split(message)
+    except ValueError:
+        return message
+    return list(filter(lambda x: len(x) > 0, split))
