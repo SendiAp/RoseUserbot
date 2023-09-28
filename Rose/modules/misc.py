2df1e504cbf60c95bfa824e253f0badb1e91ae16
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-09-29 00:31:49 +0700

    Create misc.py

---
+++ Rose/modules/misc.py
@@ -0,0 +1,80 @@
+from .. import SUDOERS
+from pyrogram.types import *
+from traceback import format_exc
+
+
+def cb_wrapper(func):
+    async def wrapper(bot, cb):
+        from .. import bot
+        users = SUDOERS
+        if cb.from_user.id not in users:
+            await cb.answer(
+                "❎ You Are Not A Sudo User❗",
+                cache_time=0,
+                show_alert=True,
+            )
+        else:
+            try:
+                await func(bot, cb)
+            except Exception:
+                print(format_exc())
+                await cb.answer(
+                    f"❎ Something Went Wrong, Please Check Logs❗..."
+                )
+
+    return wrapper
+
+
+def inline_wrapper(func):
+    async def wrapper(bot, query):
+        from .. import bot
+        users = SUDOERS
+        if query.from_user.id not in users:
+            try:
+                button = [
+                    [
+                        InlineKeyboardButton(
+                            "🌹 Deploy Rose Userbot 🌹",
+                            url=f"https://github.com/SendiAp/Rose-Userbot"
+                        )
+                    ]
+                ]
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultPhoto(
+                                photo_url=f"https://telegra.ph/file/027283ee9defebc3298b8.png",
+                                title="🌹 Rose Userbot 🌹",
+                                thumb_url=f"https://telegra.ph/file/ed46e0424514d72d70419.jpg",
+                                description=f"🌷 Deploy Your Own Rose-Userbot 🌿...",
+                                caption=f"<b>🥀 Welcome › To › Rose 🌹\n✅ Userbot v2.0 ✨...</b>",
+                                reply_markup=InlineKeyboardMarkup(button),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                await bot.answer_inline_query(
+                    query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultArticle(
+                                title="",
+                                input_message_content=InputTextMessageContent(
+                                    f"||**🥀 Tolong, Terapkan Milik Anda Di Rose Userbot❗...\n\nRepo:** <i>https://github.com/SendiAp/RoseUserbot/</i>||"
+                                ),
+                            )
+                        )
+                    ],
+                )
+            except Exception as e:
+                print(str(e))
+                pass
+        else:
+           return await func(bot, query)
+
+    return wrapper
