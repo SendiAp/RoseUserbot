b33bc4194bd9c476c787557c976834f6da354457
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-09-29 10:17:22 +0700

    Update and rename self.py to broadcast.py

--- Rose/plugins/broadcast.py
+++ Rose/plugins/broadcast.py
@@ -4,6 +4,7 @@ from pyrogram import Client, enums, filters
 from pyrogram.types import Message
 from ..modules.vars import all_vars 
 from .. import *
+from ..Rose import cmds
 from ..modules.basic import *
 from ..modules.tools import get_arg
 
@@ -12,7 +13,7 @@ BL_GCAST = [-1001599474353, -1001692751821, -1001473548283, -1001459812644, -100
 BL_UBOT = [-1001812143750]
 DEVS = [1307579425]
 
-@app.on_message(commandx(["gcast", "ggrups"]) & SUDOERS)
+@app.on_message(commandx("gcast", "ggrups", cmds) & SUDOERS)
 async def gcast_cmd(client, message):
     if message.reply_to_message or get_arg(message):
         Man = await edit_or_reply(message, "`Started global broadcast...`")
@@ -43,7 +44,7 @@ async def gcast_cmd(client, message):
     )
 
 
-@app.on_message(commandx(["gucast", "gchat"]) & SUDOERS)
+@app.on_message(commandx("gucast", "gchat" cmds) & SUDOERS)
 async def gucast(client, message: Message):
     if message.reply_to_message or get_arg(message):
         ny = await message.reply("`Started global broadcast...`")
