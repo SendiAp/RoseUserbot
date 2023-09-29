163a15926b2c51007a4b2c81d806d9292669bbe1
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-09-29 22:15:55 +0700

    Update alive.py

--- Rose/plugins/alive.py
+++ Rose/plugins/alive.py
@@ -5,6 +5,30 @@ import asyncio
 from pyrogram import Client
 from pyrogram import filters, __version__ as pyrover, enums
 
+async def get_readable_time(seconds: int) -> str:    
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += f"{time_list.pop()}, "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+    
 StartTime = time.time()
 
 async def alive():
