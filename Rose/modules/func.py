2a8ac06a626e7dbefe9e9d721f833b0db5b3884f
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-09-30 08:35:08 +0700

    Update func.py

--- Rose/modules/func.py
+++ Rose/modules/func.py
@@ -4,6 +4,16 @@ from pyrogram.types import Message
 from pytgcalls.types import *
 from pytgcalls.types.stream import *
 
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    apa = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await apa(*args, **kwargs)
+
+
+eor = edit_or_reply
 
 async def put_que(chat_id, file, type):
     put = {
