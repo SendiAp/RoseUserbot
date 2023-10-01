02f1655e8d2a704074d2bf4f89c32f84b15f1b13
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-02 01:34:08 +0700

    Update __init__.py

--- Rose/__init__.py
+++ Rose/__init__.py
@@ -61,7 +61,6 @@ babi = Client(
     name="babi",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
+    session_string=STRING_SESSION,
     plugins=dict(root="Rose/modules"),
-    in_memory=True,
 )
