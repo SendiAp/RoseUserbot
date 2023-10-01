9f6272091c5493a6cc40f47a746604fd71b72b63
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-01 12:09:43 +0700

    Update __init__.py

--- Rose/modules/__init__.py
+++ Rose/modules/__init__.py
@@ -1,9 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from pyrogram.filters import chat
-from pyrogram import Client
+from pyrogram import Client, enums 
 from typing import Dict, List, Union
 from datetime import datetime, timedelta
+from pyrogram.types import Message, User
 import pymongo.errors
+from re import sub
+from time import time
 from platform import python_version as py
 from pyrogram import __version__ as pyro
 import asyncio
@@ -11,6 +14,7 @@ import codecs
 import pickle
 import math
 import os
+import sys
 import dotenv
 import heroku3
 import requests
@@ -27,6 +31,8 @@ db = mongo.app
 
 blchatdb = db.blchat
 afkdb = db.afk
+
+admins_in_chat = {}
 
 async def blacklisted_chats(user_id: int) -> list:
     chats_list = []
@@ -57,4 +63,131 @@ async def no_afk(user_id: int):
 async def check_afk(user_id: int):
     user_data = await afkdb.users.find_one({"user_id": user_id, "afk": True})
     return user_data
-      
+
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "ProjectMan"])
+
+
+async def list_admins(client: Client, chat_id: int):
+    global admins_in_chat
+    if chat_id in admins_in_chat:
+        interval = time() - admins_in_chat[chat_id]["last_updated_at"]
+        if interval < 3600:
+            return admins_in_chat[chat_id]["data"]
+
+    admins_in_chat[chat_id] = {
+        "last_updated_at": time(),
+        "data": [
+            member.user.id
+            async for member in client.get_chat_members(
+                chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS
+            )
+        ],
+    }
+    return admins_in_chat[chat_id]["data"]
+
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+
+    text = text.strip()
+
+    if is_int(text):
+        return int(text)
+
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
+
+
+async def extract_args(message, markdown=True):
+    if not (message.text or message.caption):
+        return ""
+
+    text = message.text or message.caption
+
+    text = text.markdown if markdown else text
+    if " " not in text:
+        return ""
+
+    text = sub(r"\s+", " ", text)
+    text = text[text.find(" ") :].strip()
+    return text
+
+
+async def extract_args_arr(message, markdown=True):
+    return extract_args(message, markdown).split()
+
+
+async def get_ub_chats(
+    client: Client,
+    chat_types: list = [
+        enums.ChatType.GROUP,
+        enums.ChatType.SUPERGROUP,
+        enums.ChatType.CHANNEL,
+    ],
+    is_id_only=True,
+):
+    ub_chats = []
+    async for dialog in client.get_dialogs():
+        if dialog.chat.type in chat_types:
+            if is_id_only:
+                ub_chats.append(dialog.chat.id)
+            else:
+                ub_chats.append(dialog.chat)
+        else:
+            continue
+    return ub_chats
