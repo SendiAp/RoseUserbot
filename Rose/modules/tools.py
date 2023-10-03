d8abc4fef4ab0cc8dec77368df73ad12b1e1012a
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-03 22:53:51 +0700

    Update tools.py

--- Rose/modules/tools.py
+++ Rose/modules/tools.py
@@ -6,6 +6,78 @@ from typing import Tuple
 
 from PIL import Image
 from pyrogram.types import Message
+import sys
+from re import sub
+from time import time
+
+from pyrogram import Client, enums
+
+admins_in_chat = {}
+
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+
+    text = text.strip()
+
+    if is_int(text):
+        return int(text)
+
+    entities = message.entities
+    app = message._client
+    if len(entities) < 2:
+        return (await app.get_users(text)).id
+    entity = entities[1]
+    if entity.type == "mention":
+        return (await app.get_users(text)).id
+    if entity.type == "text_mention":
+        return entity.user.id
+    return None
+
+
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
 
 
 def get_text(message: Message) -> [None, str]:
