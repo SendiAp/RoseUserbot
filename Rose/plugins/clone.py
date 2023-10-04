b0ebc1b96d145758e35adba31498479cbefc5e6d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 09:59:26 +0700

    Update clone.py

--- Rose/plugins/clone.py
+++ Rose/plugins/clone.py
@@ -17,15 +17,15 @@ async def clone(client, message):
     text = get_text(message)
     op = await edit_or_reply(message, "`Cloning`")
     userk = get_user(message, text)[0]
-    user_ = await client.get_users(userk)
-    if not user_:
-        await op.edit("`Whom i should clone:(`")
+    user = await client.get_users(user_id)
+    if not user:
+        await op.edit("`Siapa yang harus saya tiru:(`")
         return
 
     get_bio = await client.get_chat(user_.id)
     f_name = user_.first_name
     c_bio = get_bio.bio
-    pic = photo.big_file_id(user_.id)
+    pic = user.photo.big_file_id if user.photo else None
     poto = await client.download_media(pic)
 
     await client.set_profile_photo(photo=poto)
