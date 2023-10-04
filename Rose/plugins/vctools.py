6ecfc5d83c3ba61fa873028a53d08cd680ac630d
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 23:38:29 +0700

    Update vctools.py

--- Rose/plugins/vctools.py
+++ Rose/plugins/vctools.py
@@ -89,7 +89,7 @@ async def joinvc(client: Client, message: Message):
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
-        await message.group_call.start(chat_id)
+        await group_call.start(chat_id)
     except Exception as e:
         return await Ros.edit(f"**ERROR:** `{e}`")
     await Ros.edit(f"**Berhasil Join Ke Obrolan Group**\n└ **Chat ID:** `{chat_id}`")
@@ -108,7 +108,7 @@ async def leavevc(client: Client, message: Message):
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
-        await message.group_call.stop()
+        await group_call.stop()
     except Exception as e:
         return await edit_or_reply(message, f"**ERROR:** `{e}`")
     msg = "**Berhasil Turun dari Obrolan Suara**"
