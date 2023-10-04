a955f624a1025c75300adf47d0a35d71de8a071b
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 19:57:37 +0700

    Create tiny.py

---
+++ Rose/plugins/tiny.py
@@ -0,0 +1,11 @@
+from ..import *
+
+__NAME__ = "tiny"
+__MENU__ = f"""
+**🥀 Memify Command:**
+
+`.tiny` [reply ke foto/sticker]
+Untuk Mengubah Sticker Menjadi Kecil.
+        
+© Rose Userbot
+"""
