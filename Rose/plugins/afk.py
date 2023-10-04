b50e58726f823106de755197f4df9cfa80e9292c
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 10:55:54 +0700

    Create afk.py

---
+++ Rose/plugins/afk.py
@@ -0,0 +1,147 @@
+import asyncio
+from datetime import datetime
+
+import humanize
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from ..modules.basic import GetChatID, ReplyCheck
+from ..import *
+
+AFK = False
+AFK_REASON = ""
+AFK_TIME = ""
+USERS = {}
+GROUPS = {}
+
+
+def subtract_time(start, end):
+    subtracted = humanize.naturaltime(start - end)
+    return str(subtracted)
+
+
+@Client.on_message(
+    ((filters.group & filters.mentioned) | filters.private) & ~filters.me & ~filters.service, group=3
+)
+async def collect_afk_messages(bot: Client, message: Message):
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME)
+        is_group = True if message.chat.type in ["supergroup", "group"] else False
+        CHAT_TYPE = GROUPS if is_group else USERS
+
+        if GetChatID(message) not in CHAT_TYPE:
+            text = (
+                f"`Beep boop. This is an automated message.\n"
+                f"I am not available right now.\n"
+                f"Last seen: {last_seen}\n"
+                f"Reason: ```{AFK_REASON.upper()}```\n"
+                f"See you after I'm done doing whatever I'm doing.`"
+            )
+            await bot.send_message(
+                chat_id=GetChatID(message),
+                text=text,
+                reply_to_message_id=ReplyCheck(message),
+            )
+            CHAT_TYPE[GetChatID(message)] = 1
+            return
+        elif GetChatID(message) in CHAT_TYPE:
+            if CHAT_TYPE[GetChatID(message)] == 50:
+                text = (
+                    f"`This is an automated message\n"
+                    f"Last seen: {last_seen}\n"
+                    f"This is the 10th time I've told you I'm AFK right now..\n"
+                    f"I'll get to you when I get to you.\n"
+                    f"No more auto messages for you`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+            elif CHAT_TYPE[GetChatID(message)] > 50:
+                return
+            elif CHAT_TYPE[GetChatID(message)] % 5 == 0:
+                text = (
+                    f"`Hey I'm still not back yet.\n"
+                    f"Last seen: {last_seen}\n"
+                    f"Still busy: ```{AFK_REASON.upper()}```\n"
+                    f"Try pinging a bit later.`"
+                )
+                await bot.send_message(
+                    chat_id=GetChatID(message),
+                    text=text,
+                    reply_to_message_id=ReplyCheck(message),
+                )
+
+        CHAT_TYPE[GetChatID(message)] += 1
+
+
+@app.on_message(commandx(["afk"]) & SUDOERS)
+async def afk_set(client, message):
+    global AFK_REASON, AFK, AFK_TIME
+
+    cmd = message.command
+    afk_text = ""
+
+    if len(cmd) > 1:
+        afk_text = " ".join(cmd[1:])
+
+    if isinstance(afk_text, str):
+        AFK_REASON = afk_text
+
+    AFK = True
+    AFK_TIME = datetime.now()
+
+    await message.delete()
+
+
+@app.on_message(commandx(["nafk"]) & SUDOERS)
+async def afk_unset(client, message):
+    global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+    if AFK:
+        last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+        await message.edit(
+            f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+            f"messages from {len(USERS) + len(GROUPS)} chats`"
+        )
+        AFK = False
+        AFK_TIME = ""
+        AFK_REASON = ""
+        USERS = {}
+        GROUPS = {}
+        await asyncio.sleep(5)
+
+    await message.delete()
+
+if AFK:
+   @Client.on_message(filters.me, group=3)
+   async def auto_afk_unset(bot: Client, message: Message):
+       global AFK, AFK_TIME, AFK_REASON, USERS, GROUPS
+
+       if AFK:
+           last_seen = subtract_time(datetime.now(), AFK_TIME).replace("ago", "").strip()
+           reply = await message.reply(
+               f"`While you were away (for {last_seen}), you received {sum(USERS.values()) + sum(GROUPS.values())} "
+               f"messages from {len(USERS) + len(GROUPS)} chats`"
+           )
+           AFK = False
+           AFK_TIME = ""
+           AFK_REASON = ""
+           USERS = {}
+           GROUPS = {}
+           await asyncio.sleep(5)
+           await reply.delete()
+
+
+__NAME__ = "afk"
+__MENU__ = f"""
+**🥀 Afk Mode.**
+
+`.afk` - **Activates AFK mode with reason as anything after .afk**
+Usage: `.afk [reason]`
+
+`nafk` - **Deactivates AFK mode.**
+
+© Rose Userbot
+"""
