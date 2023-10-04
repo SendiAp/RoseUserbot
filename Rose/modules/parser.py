6d2773516776fd2fccb702ffb759915af72cc43a
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 19:12:03 +0700

    Update parser.py

--- Rose/modules/parser.py
+++ Rose/modules/parser.py
@@ -9,16 +9,6 @@
 
 import html
 import re
-import asyncio
-import math
-import os
-import shlex
-from typing import Tuple
-
-from PIL import Image
-from pymediainfo import MediaInfo
-from pyrogram.types import Message
-
 
 def cleanhtml(raw_html):
     cleanr = re.compile("<.*?>")
@@ -38,30 +28,3 @@ def mention_html(user_id, name):
 
 def mention_markdown(user_id, name):
     return "[{}](tg://user?id={})".format(escape_markdown(name), user_id)
-
-def resize_image(image):
-    im = Image.open(image)
-    maxsize = (512, 512)
-    if (im.width and im.height) < 512:
-        size1 = im.width
-        size2 = im.height
-        if im.width > im.height:
-            scale = 512 / size1
-            size1new = 512
-            size2new = size2 * scale
-        else:
-            scale = 512 / size2
-            size1new = size1 * scale
-            size2new = 512
-        size1new = math.floor(size1new)
-        size2new = math.floor(size2new)
-        sizenew = (size1new, size2new)
-        im = im.resize(sizenew)
-    else:
-        im.thumbnail(maxsize)
-    file_name = "Sticker.png"
-    im.save(file_name, "PNG")
-    if os.path.exists(image):
-        os.remove(image)
-    return file_name
-    
