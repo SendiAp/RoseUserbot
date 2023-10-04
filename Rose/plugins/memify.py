3bb41bac2ddf11e64fe0bdc7209206ddcbce04f2
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 19:58:14 +0700

    Create memify.py

---
+++ Rose/plugins/memify.py
@@ -0,0 +1,13 @@
+from ..import *
+
+
+__NAME__ = "memify"
+__MENU__ = f"""
+**🥀 Memify Command:**
+
+`.mmf` Teks Atas ; Teks Bawah
+Balas Ke Pesan Sticker atau Foto akan Di ubah menjadi sticker teks meme yang di tentukan.
+        
+© Rose Userbot
+"""
+
