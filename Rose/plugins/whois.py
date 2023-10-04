74f728468dbd5585930a0b2de364a4052b7d8d13
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-04 20:03:04 +0700

    Update whois.py

--- Rose/plugins/whois.py
+++ Rose/plugins/whois.py
@@ -12,69 +12,6 @@ from ..modules.tools import extract_user
 from ..import *
 
 
-@app.on_message(commandx(["whois"]) & SUDOERS)
-async def who_is(client, message):
-    user_id = await extract_user(message)
-    Ros = await edit_or_reply(message, "`Processing . . .`")
-    if not user_id:
-        return await Ros.edit(
-            "**Berikan userid/username/reply untuk mendapatkan info pengguna tersebut.**"
-        )
-    try:
-        user = await client.get_users(user_id)
-        username = f"@{user.username}" if user.username else "-"
-        first_name = f"{user.first_name}" if user.first_name else "-"
-        last_name = f"{user.last_name}" if user.last_name else "-"
-        fullname = (
-            f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
-        )
-        user_details = (await client.get_chat(user.id)).bio
-        bio = f"{user_details}" if user_details else "-"
-        h = f"{user.status}"
-        if h.startswith("UserStatus"):
-            y = h.replace("UserStatus.", "")
-            status = y.capitalize()
-        else:
-            status = "-"
-        dc_id = f"{user.dc_id}" if user.dc_id else "-"
-        common = await client.get_common_chats(user.id)
-        out_str = f"""<b>USER INFORMATION:</b>
-
-🆔 <b>User ID:</b> <code>{user.id}</code>
-👤 <b>First Name:</b> {first_name}
-🗣️ <b>Last Name:</b> {last_name}
-🌐 <b>Username:</b> {username}
-🏛️ <b>DC ID:</b> <code>{dc_id}</code>
-🤖 <b>Is Bot:</b> <code>{user.is_bot}</code>
-🚷 <b>Is Scam:</b> <code>{user.is_scam}</code>
-🚫 <b>Restricted:</b> <code>{user.is_restricted}</code>
-✅ <b>Verified:</b> <code>{user.is_verified}</code>
-⭐ <b>Premium:</b> <code>{user.is_premium}</code>
-📝 <b>User Bio:</b> {bio}
-
-👀 <b>Same groups seen:</b> {len(common)}
-👁️ <b>Last Seen:</b> <code>{status}</code>
-🔗 <b>User permanent link:</b> <a href='tg://user?id={user.id}'>{fullname}</a>
-"""
-        photo_id = user.photo.big_file_id if user.photo else None
-        if photo_id:
-            photo = await client.download_media(photo_id)
-            await gather(
-                Ros.delete(),
-                client.send_photo(
-                    message.chat.id,
-                    photo,
-                    caption=out_str,
-                    reply_to_message_id=ReplyCheck(message),
-                ),
-            )
-            remove(photo)
-        else:
-            await Ros.edit(out_str, disable_web_page_preview=True)
-    except Exception as e:
-        return await Ros.edit(f"**INFO:** `{e}`")
-
-
 @app.on_message(commandx(["info"]) & SUDOERS)
 async def chatinfo_handler(client, message):
     Ros = await edit_or_reply(message, "`Processing...`")
