c468b1f4fc31d239ea9e7b3f889959a70b05b896
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-06 00:58:30 +0700

    Create profile.py

---
+++ Rose/plugins/profile.py
@@ -0,0 +1,98 @@
+import os
+from asyncio import sleep
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from ..modules.basic import edit_or_reply
+from ..modules.basic import ReplyCheck
+from ..modules.tools import extract_user
+
+from ..import *
+
+flood = {}
+
+@app.on_message(commandx(["block"]) & SUDOERS)
+async def block_user_func(client: Client, message: Message):
+    user_id = await extract_user(message)
+    Ros = await edit_or_reply(message, "`Processing . . .`")
+    if not user_id:
+        return await message.edit(
+            "Berikan User ID/Username atau reply pesan pengguna untuk membuka blokir."
+        )
+    if user_id == client.me.id:
+        return await Ros.edit("anda stress harap segera minum obat.")
+    await client.block_user(user_id)
+    umention = (await client.get_users(user_id)).mention
+    await message.edit(f"**Berhasil Memblokir** {umention}")
+
+
+@app.on_message(commandx(["unblock"]) & SUDOERS)
+async def unblock_user_func(client: Client, message: Message):
+    user_id = await extract_user(message)
+    Ros = await edit_or_reply(message, "`Processing . . .`")
+    if not user_id:
+        return await message.edit(
+            "Berikan User ID/Username atau reply pesan pengguna untuk membuka blokir."
+        )
+    if user_id == client.me.id:
+        return await Ros.edit("anda stress harap segera minum obat.")
+    await client.unblock_user(user_id)
+    umention = (await client.get_users(user_id)).mention
+    await message.edit(f"**Berhasil Membuka Blokir** {umention}")
+
+
+@app.on_message(commandx(["setname"]) & SUDOERS)
+async def setname(client: Client, message: Message):
+    Ros = await edit_or_reply(message, "`Processing . . .`")
+    if len(message.command) == 1:
+        return await Ros.edit(
+            "Berikan teks untuk ditetapkan sebagai nama telegram anda."
+        )
+    elif len(message.command) > 1:
+        name = message.text.split(None, 1)[1]
+        try:
+            await client.update_profile(first_name=name)
+            await Ros.edit(f"**Berhasil Mengubah Nama Telegram anda Menjadi** `{name}`")
+        except Exception as e:
+            await Ros.edit(f"**ERROR:** `{e}`")
+    else:
+        return await Ros.edit(
+            "Berikan teks untuk ditetapkan sebagai nama telegram anda."
+        )
+
+
+@app.on_message(commandx(["setbio"]) & SUDOERS)
+async def set_bio(client: Client, message: Message):
+    Ros = await edit_or_reply(message, "`Processing . . .`")
+    if len(message.command) == 1:
+        return await Ros.edit("Berikan teks untuk ditetapkan sebagai bio.")
+    elif len(message.command) > 1:
+        bio = message.text.split(None, 1)[1]
+        try:
+            await client.update_profile(bio=bio)
+            await Ros.edit(f"**Berhasil Mengubah BIO anda menjadi** `{bio}`")
+        except Exception as e:
+            await Ros.edit(f"**ERROR:** `{e}`")
+    else:
+        return await Ros.edit("Berikan teks untuk ditetapkan sebagai bio.")
+
+
+__NAME__ = "profile"
+__MENU__ = f"""
+**🥀 Profile Command:**
+
+`.block`
+**Untuk memblokir pengguna telegram.**
+
+`.unblock`
+**Untuk membuka pengguna yang anda blokir.**
+
+`.setname`
+**Untuk Mengganti Nama Telegram.**
+
+`.setbio` 
+**Untuk Mengganti Bio Telegram.**
+
+© Rose Userbot
+"""
