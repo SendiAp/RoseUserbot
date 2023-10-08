6da85aa23110b0f012a0c3b226bae0dd25056eae
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-09 00:41:06 +0700

    Update vctools.py

--- Rose/plugins/vctools.py
+++ Rose/plugins/vctools.py
@@ -80,7 +80,7 @@ async def end_vc_(client, message):
 
 
 @app.on_message(commandx(["joinvc"]) & SUDOERS)
-async def joinvc(client: Client, message: Message):
+async def joinvc(app, message: Message):
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
     if message.from_user.id != client.me.id:
         Ros = await message.reply("`Otw Naik...`")
@@ -89,7 +89,7 @@ async def joinvc(client: Client, message: Message):
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
-        await Ros.call.join(chat_id)
+        await app.call.join(chat_id)
     except Exception as e:
         return await Ros.edit(f"**ERROR:** `{e}`")
     await Ros.edit(f"**Berhasil Join Ke Obrolan Group**\n└ **Chat ID:** `{chat_id}`")
