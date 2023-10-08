80a86a6c4283072c0b7e6a4d83a6e7f475214e99
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-09 00:04:31 +0700

    Update vctools.py

--- Rose/plugins/vctools.py
+++ Rose/plugins/vctools.py
@@ -89,7 +89,7 @@ async def joinvc(client: Client, message: Message):
     with suppress(ValueError):
         chat_id = int(chat_id)
     try:
-        await client.call.join(chat_id)
+        await Ros.call.join(chat_id)
     except Exception as e:
         return await Ros.edit(f"**ERROR:** `{e}`")
     await Ros.edit(f"**Berhasil Join Ke Obrolan Group**\n└ **Chat ID:** `{chat_id}`")
