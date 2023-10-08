f4a06d82ebd5727b326ea4caa9a39329eed85c16
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-08 19:10:29 +0700

    Create systemstats.py

---
+++ Rose/plugins/systemstats.py
@@ -0,0 +1,85 @@
+import platform
+import sys
+from datetime import datetime
+import psutil
+from asyncio import create_subprocess_exec as asyncrunapp
+from pyrogram import filters, Client
+from pyrogram import __version__
+from pyrogram.raw.functions import Ping
+from pyrogram.types import Message
+from . import *
+from pytgcalls import __version__ as pytgcalls
+from pyrogram.enums import ParseMode 
+from datetime import datetime, timedelta
+from ..modules import *
+from ..import *
+from .ping import get_readable_time
+
+
+def get_size(bytes, suffix="B"):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+@app.on_message(commandx(["spc"]) & SUDOERS)
+async def psu(client: Client, message: Message):
+    uname = platform.uname()
+    softw = "**Informasi Sistem**\n"
+    softw += f"`Sistem   : {uname.system}`\n"
+    softw += f"`Rilis    : {uname.release}`\n"
+    softw += f"`Versi    : {uname.version}`\n"
+    softw += f"`Mesin    : {uname.machine}`\n"
+    # Boot Time
+    boot_time_timestamp = psutil.boot_time()
+    bt = datetime.fromtimestamp(boot_time_timestamp)
+    softw += f"`Waktu Hidup: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
+    # CPU Cores
+    cpuu = "**Informasi CPU**\n"
+    cpuu += "`Physical cores   : " + \
+        str(psutil.cpu_count(logical=False)) + "`\n"
+    cpuu += "`Total cores      : " + \
+        str(psutil.cpu_count(logical=True)) + "`\n"
+    # CPU frequencies
+    cpufreq = psutil.cpu_freq()
+    cpuu += f"`Max Frequency    : {cpufreq.max:.2f}Mhz`\n"
+    cpuu += f"`Min Frequency    : {cpufreq.min:.2f}Mhz`\n"
+    cpuu += f"`Current Frequency: {cpufreq.current:.2f}Mhz`\n\n"
+    # CPU usage
+    cpuu += "**CPU Usage Per Core**\n"
+    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+        cpuu += f"`Core {i}  : {percentage}%`\n"
+    cpuu += "**Total CPU Usage**\n"
+    cpuu += f"`Semua Core: {psutil.cpu_percent()}%`\n"
+    # RAM Usage
+    svmem = psutil.virtual_memory()
+    memm = "**Memori Digunakan**\n"
+    memm += f"`Total     : {get_size(svmem.total)}`\n"
+    memm += f"`Available : {get_size(svmem.available)}`\n"
+    memm += f"`Used      : {get_size(svmem.used)}`\n"
+    memm += f"`Percentage: {svmem.percent}%`\n"
+    # Bandwidth Usage
+    bw = "**Bandwith Digunakan**\n"
+    bw += f"`Unggah  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
+    bw += f"`Download: {get_size(psutil.net_io_counters().bytes_recv)}`\n"
+    help_string = f"{str(softw)}\n"
+    help_string += f"{str(cpuu)}\n"
+    help_string += f"{str(memm)}\n"
+    help_string += f"{str(bw)}\n"
+    help_string += "**Informasi Mesin**\n"
+    help_string += f"`Python {sys.version}`\n"
+    help_string += f"`Pyrogram {__version__}`\n"
+    help_string += f"**Powered by {client.me.mention}**\n"
+    await message.reply(help_string)
+
+
+__NAME__ = "sistem"
+__MENU__ = f"""
+**🥀 Sistem:**
+
+`.spc`
+**Informasi Mesin**
+
+© Rose Userbot
+"""
