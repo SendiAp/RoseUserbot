7853107562e718f4371f4a2eb0b3d4e99d0009aa
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-09 09:55:16 +0700

    Update quran.py

--- Rose/plugins/quran.py
+++ Rose/plugins/quran.py
@@ -10,6 +10,7 @@ from pyrogram import Client, errors
 from pyrogram.types import Message
 from emoji import get_emoji_regexp
 from PIL import Image, ImageDraw, ImageFont
+from ..modules.basic import edit_or_reply
 from ..modules.tools import get_arg
 from ..modules import *
 from ..import *
@@ -18,7 +19,7 @@ from ..import *
 @app.on_message(commandx(["quran"]) & SUDOERS)
 async def quran(client: Client, message: Message):
     args = get_arg(message)
-    if not message.reply_to_message and not args:
+    if not args:
         return await message.edit("**Contoh 1:3**")
     bot = "AlFurqanRobot"
     if message.reply_to_message:
@@ -34,3 +35,18 @@ async def quran(client: Client, message: Message):
         quran_msg = response.updates[1].message.id + 1
         status = await app.get_messages(chat_id="AlFurqanRobot", message_ids=quran_msg)
         await message.edit(f"{status.text}")
+
+
+@app.on_message(commandx(["quranbot"]) & SUDOERS)
+async def quran_bot(client: Client, message: Message):
+    args = get_arg(message)
+    if not args:
+        return await message.edit("**Contoh 1:3**")
+    bot = "AlFurqanRobot"
+    await client.unblock_user(bot)
+    response = await client.send_message(bot, f"{args}")
+    Ros = await edit_or_reply(message, "`Processing...`")
+    await asyncio.sleep(1)
+    spambot_msg = response.updates[1].message.id + 1
+    status = await app.get_messages(chat_id=bot, message_ids=spambot_msg)
+    await Ros.edit(f"~ {status.text}")
