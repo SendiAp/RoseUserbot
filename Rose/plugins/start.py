3715686b230f0c1b31c94f75e05d6af654d180ce
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-15 06:26:23 +0700

    Update start.py

--- Rose/plugins/start.py
+++ Rose/plugins/start.py
@@ -1,79 +1,53 @@
-import html 
-from pyrogram import Client, filters, idle
-from pyrogram.types import Message, User
-from ..import *
-from ..modules.basic import ReplyCheck
-from ..modules.vars import *
+import asyncio
+from pyrogram import filters
+from bot import feedback
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from about import About
 from ..modules import *
-from ..modules.humanbytes import humanbytes
+from ..modules.vars import *
+from ..import *
 
-BOT_OWNER = Config.OWNER_ID
-owner_id = Config.OWNER_ID
+@bot.on_message(filters.text)
+async def text(c, m):
+      if m.from_user.id in Config.LOGIN:
+         if m.text == Config.PASS:
+            Config.LOGIN.remove(m.from_user.id)
+            Config.OWNER.append(m.from_user.id)
+            await m.reply_text(text="From now you will receive feedbacks. Untill this bot restart.  If you want to get feedbacks permanently add your id in config vars")
+         if m.text != Config.PASS:
+            Config.LOGIN.remove(m.from_user.id)
+            await m.reply_text(text="**Incorrect Password**", parse_mode="markdown")
+      if m.from_user.id in Config.feedback:
+         button = [[
+                   InlineKeyboardButton("Yes", callback_data="yes"),
+                   InlineKeyboardButton("No", callback_data="cancel")
+                  ]]
+         markup = InlineKeyboardMarkup(button)
+         await m.reply_text(text="Are you sure to send this feedback",
+                            reply_markup=markup,
+                            quote=True)
+      try:
+          if Config.SEND is not None:
+             id = Config.SEND[0]
+             await c.send_message(chat_id=int(id), text=m.text, parse_mode="markdown")
+             Config.SEND.remove(id)
+             await c.send_message(chat_id=m.chat.id, text="Notified successfully")
+      except:
+          pass
 
-IF_TEXT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
-IF_CONTENT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
-
-@bot.on_message(filters.private & filters.text)
-async def pm_text(bot, message):
-    if message.from_user.id == BOT_OWNER:
-        await reply_text(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.send_message(
-        chat_id=owner_id,
-        text=IF_TEXT.format(reference_id, info.first_name, message.text),
-        reply_to_message_id=message.id,
-    )
-
-@bot.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id == BOT_OWNER:
-        await replay_media(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.copy_message(
-        chat_id=owner_id,
-        from_chat_id=message.chat.id,
-        reply_to_message_id=message.id,
-        caption=IF_CONTENT.format(reference_id, info.first_name),
-    )
-
-@bot.on_message(filters.user(owner_id) & filters.text & filters.private)
-async def reply_text(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.send_message(
-            text=message.text,
-            chat_id=int(reference_id),
-            reply_to_message_id=message.id,
-        )  
-       
-@bot.on_message(filters.user(owner_id) & filters.media & filters.private)
-async def replay_media(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.copy_message(
-            chat_id=int(reference_id),
-            from_chat_id=message.chat.id,
-            reply_to_message_id=message.id,
-        )   
+@bot.on_message(filters.command(["start"]) & filters.private)
+async def start(c, m):
+      button = [[
+                InlineKeyboardButton("Feedback", callback_data="feedback"),
+                InlineKeyboardButton("Rules", callback_data="rules"),
+                ],
+                [
+                InlineKeyboardButton("About", callback_data="about"),
+                InlineKeyboardButton("Login", callback_data="login"),
+               ]]
+      markup = InlineKeyboardMarkup(button)
+      await c.send_message(chat_id=m.chat.id,
+                           text=Translation.START,
+                           disable_web_page_preview=True,
+                           reply_to_message_id=m.message_id,
+                           reply_markup=markup)
