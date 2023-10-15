83d8c8a26b6d5674376b57e6aa62c685f220ae9c
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-15 08:57:09 +0700

    Update start.py

--- Rose/plugins/start.py
+++ Rose/plugins/start.py
@@ -40,6 +40,58 @@ Tekan /deploy untuk melihat semua perintah dan cara kerjanya.
         ),
      disable_web_page_preview=True
     )
+	
+
+@bot.on_callback_query(filters.regex("feed"))
+async def feed(_, query: CallbackQuery):
+      Config.feedback.append(query.from_user.id)
+      button = [[InlineKeyboardButton("cancel", callback_data="cancel")]]
+      markup = InlineKeyboardMarkup(button)
+      await query.delete()
+      await query.send_message(chat_id=m.message.chat.id, text="Send your feed back here I will notify the admin.", reply_markup=markup)
+
+@bot.on_callback_query(filters.regex("cancel"))
+async def cancel(_, query: CallbackQuery):
+      if query.from_user.id in Config.feedback:
+         Config.feedback.remove(m.from_user.id)
+      if query.from_user.id in Config.LOGIN:
+         Config.LOGIN.remove(m.from_user.id)
+      await query.delete()
+      await start(query, query.message)
+
+@bot.on_callback_query(filters.regex("rules"))
+async def rules(_, query: CallbackQuery):
+      await query.delete()
+      await query.send_message(chat_id=m.message.chat.id, text=Config.RULES)
+
+@bot.on_callback_query(filters.regex("login"))
+async def login(_, query: CallbackQuery):
+      Config.LOGIN.append(m.from_user.id)
+      await query.delete()
+      await query.send_message(chat_id=m.message.chat.id, text=Config.LOGIN)
+       
+@bot.on_callback_query(filters.regex("yes"))
+async def yes(_, query: CallbackQuery):
+      Config.feedback.remove(m.from_user.id)
+      feedtext = query.message.reply_to_message
+      button = [[InlineKeyboardButton("Reply", callback_data=f"reply+{query.from_user.id}")]]
+      markup = InlineKeyboardMarkup(button)
+      for i in Config.OWNER:
+          NS = await feedtext.forward(int(i))
+          await NS.reply_text("Send the reply", reply_markup=markup, quote=True)
+      await query.delete()
+      await query.send_message(chat_id=m.message.chat.id, text="Feedback sent successfully. Hope you will get reply soon")
+  
+@bot.on_callback_query(filters.regex("reply"))
+async def reply(_, query: CallbackQuery):
+      id = m.data.split("+")[1]
+      Config.SEND.append(id)
+      await query.send_message(chat_id=m.message.chat.id, text="Reply me the text which you wanted to send us")
+
+@bot.on_callback_query(filters.regex("about"))
+async def about(_, query: CallbackQuery):
+      await query.delete()
+      await query.send_message(chat_id=m.message.chat.id, text=About.ABOUT, disable_web_page_preview=True)
 
 
 @bot.on_message(filters.text)
