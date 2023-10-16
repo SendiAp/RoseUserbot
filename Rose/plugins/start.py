cc8fd1d216dc22957b37a90b5124edd3168b4784
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-16 10:52:34 +0700

    Update start.py

--- Rose/plugins/start.py
+++ Rose/plugins/start.py
@@ -19,23 +19,8 @@ async def pm_text(bot, message):
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
     await bot.send_message(
-        chat_id=owner_id,
+        message.chat.id=owner_id,
         text=IF_TEXT.format(reference_id, info.first_name, message.text),
-        parse_mode=enums.ParseMode.HTML,
-    )
-
-@bot.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id == owner_id:
-        await replay_media(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.copy_message(
-        chat_id=owner_id,
-        from_chat_id=message.chat.id,
-        message_id=message.message_id,
-        caption=IF_CONTENT.format(reference_id, info.first_name),
         parse_mode=enums.ParseMode.HTML,
     )
 
@@ -57,22 +42,3 @@ async def reply_text(bot, message):
             chat_id=int(reference_id)
         )  
        
-@bot.on_message(filters.user(owner_id) & filters.media & filters.private)
-async def replay_media(bot, message):
-    reference_id = True
-    if message.reply_to_message is not None:
-        file = message.reply_to_message
-        try:
-            reference_id = file.text.split()[2]
-        except Exception:
-            pass
-        try:
-            reference_id = file.caption.split()[2]
-        except Exception:
-            pass
-        await bot.copy_message(
-            chat_id=int(reference_id),
-            from_chat_id=message.chat.id,
-            message_id=message.message_id,
-            parse_mode=enums.ParseMode.HTML,
-        )   
