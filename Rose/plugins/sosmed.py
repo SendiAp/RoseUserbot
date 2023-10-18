f1fdeec2ac646e74363d1cf6a28792d756300bbd
SendiAdt <86170325+SendiAp@users.noreply.github.com>
2023-10-18 18:07:50 +0700

    Create sosmed.py

---
+++ Rose/plugins/sosmed.py
@@ -0,0 +1,13 @@
+from .. import *
+
+
+__NAME__ = "sosmed"
+__MENU__ = f"""
+✘ **Perintah:** `{cmds}sosmed` [link]
+• **Fungsi:** Download Video From Tiktok Instagram Dll.
+
+✘ **Perintah:** `{cmds}tt` [link]
+• **Fungsi:** Download Video from Tiktok.
+
+© Rose Userbot
+"""
